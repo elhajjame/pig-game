8649a5f79cbce09b72346d4a2c578eb5b2ca6580
mehdi el-hajjame <el-hajjame@outlook.com>
2025-12-15 18:00:18 +0100

    roll function

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+
+const score0Elm = document.getElementById('score--0');
+const score1Elm = document.getElementById('score--1');
+
+const currentScore0 = document.querySelector('.current--0');
+const currentScore1 = document.querySelector('.current--1');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+let diceEl = document.querySelector('.dice');
+
+diceEl.classList.add('hidden');
+
+const rollDice = function(){
+        diceEl.classList.remove('hidden')
+
+    const diceNum = Math.floor(Math.random() * 6) + 1;
+    console.log(diceNum);
+
+    diceEl.src = `images/dice-${diceNum}.png`
+}
+btnRoll.addEventListener('click', rollDice)
