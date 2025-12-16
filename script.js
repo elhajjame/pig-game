d24b268427a90a052b58f9bbc4fa4f41808cd5e7
mehdi el-hajjame <el-hajjame@outlook.com>
2025-12-16 15:52:05 +0100

    calculate score function

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 'use strict';
 
 
-const score0Elm = document.getElementById('score--0');
-const score1Elm = document.getElementById('score--1');
+let score0Elm = document.getElementById('score--0');
+let score1Elm = document.getElementById('score--1');
+score0Elm = 0;
+score1Elm = 0;
 
-const currentScore0 = document.querySelector('.current--0');
+let currentScore0 = document.querySelector('.current--0');
 const currentScore1 = document.querySelector('.current--1');
 
 const btnNew = document.querySelector('.btn--new');
@@ -14,12 +16,22 @@ let diceEl = document.querySelector('.dice');
 
 diceEl.classList.add('hidden');
 
-const rollDice = function(){
-        diceEl.classList.remove('hidden')
+const rollDice = function () {
+    diceEl.classList.remove('hidden')
 
     const diceNum = Math.floor(Math.random() * 6) + 1;
     console.log(diceNum);
 
     diceEl.src = `images/dice-${diceNum}.png`
+
+    if (diceNum !== 1) {
+
+            currentScore0 += diceNum 
+            console.log('test  ', currentScore0);
+            } else if (diceNum === "1" ){
+
+                currentScore0 = 0;
+                console.log('test  ', currentScore0);
+            }
 }
-btnRoll.addEventListener('click', rollDice)
+btnRoll.addEventListener('click', rollDice);
