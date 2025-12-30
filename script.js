7bd2c654d4cdcc669a6d046ca99956c0b1477ed9
mehdi el-hajjame <el-hajjame@outlook.com>
2025-12-30 12:27:05 +0100

    all done and the game is working now

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 'use strict';
 
-
 let current0El = document.getElementById('current--0');
+let current1El = document.getElementById('current--1');
 let player0El = document.querySelector('.player--0');
 let player1El = document.querySelector('.player--1');
 let score0Elm = document.getElementById('score--0');
 let score1Elm = document.getElementById('score--1');
 let diceEl = document.querySelector('.dice');
+
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-score0Elm.textContent = 0;
-score1Elm.textContent = 0;
 
-let score = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-diceEl.classList.add('hidden');
-let gameStatus = true;
+let score, currentScore, activePlayer, gameStatus;
+
+const init = function () {
+    score = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    gameStatus = true;
+
+    score0Elm.textContent = 0;
+    score1Elm.textContent = 0;
+
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+}
+
+init();
 
 const switchPlayer = function () {
     currentScore = 0;
@@ -26,28 +43,27 @@ const switchPlayer = function () {
     player0El.classList.toggle('player--active')
     player1El.classList.toggle('player--active')
 }
+
 const rollDice = function () {
     if (gameStatus) {
 
         const diceNum = Math.floor(Math.random() * 6) + 1;
         diceEl.classList.remove('hidden');
-        // console.log(diceNum);
 
         diceEl.src = `images/dice-${diceNum}.png`
 
         if (diceNum !== 1) {
             currentScore += diceNum
             console.log('test ', currentScore);
-            // current0El.textContent = currentScore;
+
             document.getElementById(`current--${activePlayer}`).textContent = currentScore;
         } else {
             switchPlayer();
         }
     }
 }
-btnRoll.addEventListener('click', rollDice);
 
-btnHold.addEventListener('click', function () {
+const gameWinner = function () {
     if (gameStatus) {
 
         score[activePlayer] += currentScore;
@@ -64,5 +80,8 @@ btnHold.addEventListener('click', function () {
             switchPlayer();
         }
     }
-})
+}
 
+btnRoll.addEventListener('click', rollDice);
+btnHold.addEventListener('click', gameWinner)
+btnNew.addEventListener('click', init);
